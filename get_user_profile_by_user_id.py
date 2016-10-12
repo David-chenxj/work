c6cb0155113ece7a1163d718834aca6216c35319
David-chenxj <box_for_mail@126.com>
2016-10-12 11:29:19 +0800

    Update get_user_profile_by_user_id.py
    adjust output user profile us and id  file name to keep the same format.

--- get_user_profile_by_user_id.py
+++ get_user_profile_by_user_id.py
@@ -6,7 +6,7 @@ TOTAL_DAYS=60
 DUMP_CMD = "/app/apache-hive-1.2.1-bin/bin/hive"
 
 def dump(date_str,user_id):
-	cmd_str = """%s -e "use opera; select a.label, a.user_id, a.news_entry_id, b.user_profile, b.news_profile from ml_training_impr_click_joined as a join news_running_feature_by_country b on a.dt=b.dt and a.country=b.country and a.user_id=b.user_id and a.request_id=b.request_id and a.news_entry_id=b.news_entry_id where a.dt='%s' and a.country='id' and a.user_id = '%s';" >> /mnt1/chenxj/new_dump/%s_%sid_unf_ffm.feature""" % (DUMP_CMD, date_str,user_id, user_id,date_str)
+	cmd_str = """%s -e "use opera; select a.label, a.user_id, a.news_entry_id, b.user_profile, b.news_profile from ml_training_impr_click_joined as a join news_running_feature_by_country b on a.dt=b.dt and a.country=b.country and a.user_id=b.user_id and a.request_id=b.request_id and a.news_entry_id=b.news_entry_id where a.dt='%s' and a.country='id' and a.user_id = '%s';" >> /mnt1/chenxj/new_dump/%s_%s_id_unf_ffm.feature""" % (DUMP_CMD, date_str,user_id, user_id,date_str)
 	os.popen(cmd_str, 'r')
 	cmd_str = """%s -e "use opera; select a.label, a.user_id, a.news_entry_id, b.user_profile, b.news_profile from ml_training_impr_click_joined as a join news_running_feature_by_country b on a.dt=b.dt and a.country=b.country and a.user_id=b.user_id and a.request_id=b.request_id and a.news_entry_id=b.news_entry_id where a.dt='%s' and a.country='us' and a.user_id = '%s';" >> /mnt1/chenxj/new_dump/%s_%s_us_unf_ffm.feature""" % (DUMP_CMD, date_str, user_id,user_id,date_str)
 	os.popen(cmd_str, 'r')
